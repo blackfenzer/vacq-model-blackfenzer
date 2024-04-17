a2a25e6a88a31fde9e1ebc6938da69bef3cba23a
blackfenzer <6431346921@student.chula.ac.th>
2024-04-17 17:08:10 +0700

    fixx status

--- vacqbackend/controllers/hospitals.js
+++ vacqbackend/controllers/hospitals.js
@@ -90,7 +90,7 @@ exports.getHospital = async (req, res, next) => {
 
 exports.createHospital = async (req, res, next) => {
   const newHospital = await Hospital.create(req.body);
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: newHospital,
   });
