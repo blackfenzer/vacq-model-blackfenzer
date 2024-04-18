2f3dcdcbca1efacc8e81825685b84023081baea3
blackfenzer <6431346921@student.chula.ac.th>
2024-04-18 16:34:28 +0700

    fix final assignment

--- vacqbackend/controllers/appointments.js
+++ vacqbackend/controllers/appointments.js
@@ -1,4 +1,5 @@
 const Appointment = require("../models/Appointment");
+const Hospital = require("../models/Hospital");
 
 exports.getAppointments = async (req, res, next) => {
   let query;
@@ -37,25 +38,39 @@ exports.getAppointments = async (req, res, next) => {
 
 exports.addAppointment = async (req, res, next) => {
   try {
-    req.body.user = req.user.id;
+    req.body.hospital = req.params.hospitalId;
 
     const hospital = await Hospital.findById(req.params.hospitalId);
 
     if (!hospital) {
-      return res
-        .status(404)
-        .json({
-          success: false,
-          message: `No hospital with the id of ${req.params.hospitalId}`,
-        });
+      return res.status(404).json({
+        success: false,
+        message: `No hospital with the id of ${req.params.hospitalId}`,
+      });
+    }
+
+    //add user Id to req.body
+    req.body.user = req.user.id;
+
+    //Check for existed appointment
+    const existedAppointment = await Appointment.find({ user: req.user.id });
+
+    //If the user is not an admin, they can only create 3 appointment.
+    if (existedAppointment.length >= 3 && req.user.role !== "admin") {
+      return res.status(404).json({
+        success: false,
+        message: `The user with id ${req.user.id} has already made 3 appointments`,
+      });
     }
 
     const appointment = await Appointment.create(req.body);
 
-    res.status(200).json({ success: true, data: appointment });
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
   } catch (error) {
     console.log(error);
-
     return res
       .status(500)
       .json({ success: false, message: "Cannot create Appointment" });
